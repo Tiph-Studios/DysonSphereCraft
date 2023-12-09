e4f2d39d4b5d7408d814de8480c92b2d846ec719
zhouzhouthezhou <kylezhou2222@gmail.com>
2023-12-09 00:46:44 -0800

    reformat code to comply with google java

--- src/main/java/com/tiph/dysonsphere/Config.java
+++ src/main/java/com/tiph/dysonsphere/Config.java
@@ -1,5 +1,8 @@
 package com.tiph.dysonsphere;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
@@ -8,46 +11,35 @@ import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.config.ModConfigEvent;
 import net.neoforged.neoforge.common.ModConfigSpec;
 
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 // An example config class. This is not required, but it's a good idea to have one to keep your config organized.
 // Demonstrates how to use Forge's config APIs
 @Mod.EventBusSubscriber(modid = ExampleMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class Config
 {
 
-    private Config() {
-        // Do not instantiate
-        throw new UnsupportedOperationException("Do not instantiate");
-    }
-
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
-
-    private static final ModConfigSpec.BooleanValue LOG_DIRT_BLOCK = BUILDER
-            .comment("Whether to log the dirt block on common setup")
-            .define("logDirtBlock", true);
-
-    private static final ModConfigSpec.IntValue MAGIC_NUMBER = BUILDER
-            .comment("A magic number")
-            .defineInRange("magicNumber", 42, 0, Integer.MAX_VALUE);
-
     public static final ModConfigSpec.ConfigValue<String> MAGIC_NUMBER_INTRODUCTION = BUILDER
             .comment("What you want the introduction message to be for the magic number")
             .define("magicNumberIntroduction", "The magic number is... ");
-
+    static final ModConfigSpec SPEC = BUILDER.build();
+    private static final ModConfigSpec.BooleanValue LOG_DIRT_BLOCK = BUILDER
+            .comment("Whether to log the dirt block on common setup")
+            .define("logDirtBlock", true);
+    private static final ModConfigSpec.IntValue MAGIC_NUMBER = BUILDER
+            .comment("A magic number")
+            .defineInRange("magicNumber", 42, 0, Integer.MAX_VALUE);
     // a list of strings that are treated as resource locations for items
     private static final ModConfigSpec.ConfigValue<List<? extends String>> ITEM_STRINGS = BUILDER
             .comment("A list of items to log on common setup.")
             .defineListAllowEmpty("items", List.of("minecraft:iron_ingot"), Config::validateItemName);
-
-    static final ModConfigSpec SPEC = BUILDER.build();
-
     private static boolean logDirtBlock;
     private static int magicNumber;
     private static String magicNumberIntroduction;
     private static Set<Item> items;
+    private Config() {
+        // Do not instantiate
+        throw new UnsupportedOperationException("Do not instantiate");
+    }
 
     private static boolean validateItemName(final Object obj)
     {
