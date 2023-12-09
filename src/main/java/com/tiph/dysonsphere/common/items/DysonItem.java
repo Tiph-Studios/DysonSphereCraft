8cd8da94e9ab4b7b8f53b399ef25df667112b35d
Connor Pashak <18432348+cmpashak@users.noreply.github.com>
2023-12-09 13:22:46 -0700

    mirror?

--- src/main/java/com/tiph/dysonsphere/common/items/DysonItem.java
+++ src/main/java/com/tiph/dysonsphere/common/items/DysonItem.java
@@ -3,11 +3,16 @@ package com.tiph.dysonsphere.common.items;
 import com.tiph.dysonsphere.ExampleMod;
 import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public abstract class DysonItem extends Item {
 
   public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(ExampleMod.MODID);
+
+
+      public static final DeferredItem<Item> MIRROR_ITEM = ITEMS.registerSimpleItem("mirror_1",
+              new Item.Properties());
 
   public DysonItem(final Properties p) {
     super(p);
