18c449cf40febcc43f14c9f83822bac1c4e93572
zhouzhouthezhou <kylezhou2222@gmail.com>
2023-12-10 22:30:19 -0800

    make solar generator an entity

--- src/main/java/com/tiph/dysonsphereproject/common/blocks/SolarGenerator.java
+++ src/main/java/com/tiph/dysonsphereproject/common/blocks/SolarGenerator.java
@@ -1,13 +1,18 @@
 package com.tiph.dysonsphereproject.common.blocks;
 
+import com.tiph.dysonsphereproject.common.blocks.entities.SolarGeneratorEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class SolarGenerator extends Block {
+import javax.annotation.Nullable;
+
+public class SolarGenerator extends Block implements EntityBlock {
   private static final String REGISTRY_SUFFIX = "solar_generator";
   private static final float DESTROY_TIME = 2.0f;
   private static final VoxelShape SHAPE = Block.box(0, 0, 0, 16.0, 8.0, 16.0);
@@ -20,6 +25,12 @@ public class SolarGenerator extends Block {
 
   public SolarGenerator(Properties p) {
     super(p);
+  }
+
+  @Nullable
+  @Override
+  public BlockEntity newBlockEntity(BlockPos pos, BlockState state){
+    return new SolarGeneratorEntity(pos, state);
   }
 
   public static String getRegistrySuffix() {
--- src/main/java/com/tiph/dysonsphereproject/common/init/DysonBlocks.java
+++ src/main/java/com/tiph/dysonsphereproject/common/init/DysonBlocks.java
@@ -4,11 +4,14 @@ import com.tiph.dysonsphereproject.DysonSphereProject;
 import com.tiph.dysonsphereproject.common.api.IResource;
 import com.tiph.dysonsphereproject.common.blocks.BasicBlocks;
 import com.tiph.dysonsphereproject.common.blocks.SolarGenerator;
+import com.tiph.dysonsphereproject.common.blocks.entities.SolarGeneratorEntity;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.function.Supplier;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredBlock;
@@ -21,9 +24,14 @@ public class DysonBlocks {
   public static final DeferredRegister.Blocks BLOCKS =
       DeferredRegister.createBlocks(DysonSphereProject.MODID);
 
+  public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES =
+      DeferredRegister.create(BuiltInRegistries.BLOCK_ENTITY_TYPE, DysonSphereProject.MODID);
+
   private static final Map<IResource, DeferredBlock<Block>> BASIC_BLOCKS = new LinkedHashMap<>();
 
+  ///////////////////
   // Basic Blocks
+  ///////////////////
   static {
     for (BasicBlocks block : BasicBlocks.values()) {
       BASIC_BLOCKS.put(block, registerBasicBlock(block));
@@ -38,8 +46,16 @@ public class DysonBlocks {
               new SolarGenerator(
                   BlockBehaviour.Properties.of().destroyTime(SolarGenerator.getDestroyTime())));
 
+
+  ///////////////////
   // Tile Entities
-  // Add Tile Entities here
+  ///////////////////
+  public static final Supplier<BlockEntityType<SolarGeneratorEntity>> SOLAR_GENERATOR_ENTITY =
+      BLOCK_ENTITIES.register(
+          "solar_generator",
+          () ->
+              BlockEntityType.Builder.of(SolarGeneratorEntity::new, SOLAR_GENERATOR.get())
+                  .build(null));
 
   private static DeferredBlock<Block> registerBasicBlock(BasicBlocks block) {
     return registerBlockAndItem(
