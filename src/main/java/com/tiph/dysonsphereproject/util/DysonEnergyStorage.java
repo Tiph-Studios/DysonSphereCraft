77644c449762ea00e9bc036ae278672789f5bbe8
Connor Pashak <18432348+cmpashak@users.noreply.github.com>
2023-12-16 14:48:35 -0700

    energy is good

---
+++ src/main/java/com/tiph/dysonsphereproject/util/DysonEnergyStorage.java
@@ -0,0 +1,10 @@
+package com.tiph.dysonsphereproject.util;
+
+import net.neoforged.neoforge.energy.EnergyStorage;
+
+public class DysonEnergyStorage extends EnergyStorage {
+
+    public DysonEnergyStorage(int capacity, int maxReceive, int maxExtract) {
+        super(capacity, maxReceive, maxExtract);
+    }
+}
