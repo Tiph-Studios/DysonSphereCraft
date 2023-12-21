02c849fd4a42e89f8c689363348ddd2e8d1fb91e
Connor Pashak <18432348+cmpashak@users.noreply.github.com>
2023-12-20 21:37:19 -0700

    end me

---
+++ src/main/java/com/tiph/dysonsphereproject/mixins/OverworldSkyMixin.java
@@ -0,0 +1,22 @@
+package com.tiph.dysonsphereproject.mixins;
+
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.renderer.DimensionSpecialEffects;
+import org.joml.Vector3f;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(DimensionSpecialEffects.OverworldEffects.class)
+public abstract class OverworldSkyMixin extends DimensionSpecialEffects {
+
+    public OverworldSkyMixin(float p_108866_, boolean p_108867_, SkyType p_108868_, boolean p_108869_, boolean p_108870_) {
+        super(p_108866_, p_108867_, p_108868_, p_108869_, p_108870_);
+    }
+
+    @Override
+    public void adjustLightmapColors(ClientLevel level, float partialTicks, float skyDarken, float blockLightRedFlicker, float skyLight, int pixelX, int pixelY, Vector3f colors) {
+        colors = new Vector3f(255);
+
+        this.
+        super.adjustLightmapColors(level, partialTicks, skyDarken, blockLightRedFlicker, skyLight, pixelX, pixelY, colors);
+    }
+}
--- src/main/java/com/tiph/dysonsphereproject/mixins/TestMixin.java
+++
@@ -1,6 +0,0 @@
-package com.tiph.dysonsphereproject.mixins;
-
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin
-public class TestMixin {}
