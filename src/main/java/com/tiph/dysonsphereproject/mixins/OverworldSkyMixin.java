5724785a4bb54e7b5323d783f75148ebf1f7195a
Connor Pashak <18432348+cmpashak@users.noreply.github.com>
2023-12-22 09:50:35 -0700

    format

--- src/main/java/com/tiph/dysonsphereproject/mixins/OverworldSkyMixin.java
+++ src/main/java/com/tiph/dysonsphereproject/mixins/OverworldSkyMixin.java
@@ -8,14 +8,24 @@ import org.spongepowered.asm.mixin.Mixin;
 @Mixin(DimensionSpecialEffects.OverworldEffects.class)
 public abstract class OverworldSkyMixin extends DimensionSpecialEffects {
 
-    public OverworldSkyMixin(float p_108866_, boolean p_108867_, SkyType p_108868_, boolean p_108869_, boolean p_108870_) {
-        super(p_108866_, p_108867_, p_108868_, p_108869_, p_108870_);
-    }
+  public OverworldSkyMixin(
+      float p_108866_, boolean p_108867_, SkyType p_108868_, boolean p_108869_, boolean p_108870_) {
+    super(p_108866_, p_108867_, p_108868_, p_108869_, p_108870_);
+  }
 
-    @Override
-    public void adjustLightmapColors(ClientLevel level, float partialTicks, float skyDarken, float blockLightRedFlicker, float skyLight, int pixelX, int pixelY, Vector3f colors) {
-        colors = new Vector3f(255);
-        
-//        super.adjustLightmapColors(level, partialTicks, skyDarken, blockLightRedFlicker, skyLight, pixelX, pixelY, colors);
-    }
+  @Override
+  public void adjustLightmapColors(
+      ClientLevel level,
+      float partialTicks,
+      float skyDarken,
+      float blockLightRedFlicker,
+      float skyLight,
+      int pixelX,
+      int pixelY,
+      Vector3f colors) {
+    colors = new Vector3f(255);
+
+    //        super.adjustLightmapColors(level, partialTicks, skyDarken, blockLightRedFlicker,
+    // skyLight, pixelX, pixelY, colors);
+  }
 }
